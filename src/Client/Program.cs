dc4c30e316fa13398a8b0621b112d8b5012818cd
Benjamin Vertonghen <benjamin.vertonghen@append.be>
2022-10-15 11:39:35 +0200

    Update to .Net 6

--- src/Client/Program.cs
+++ src/Client/Program.cs
@@ -1,25 +1,15 @@
-using System;
-using System.Net.Http;
-using System.Collections.Generic;
-using System.Threading.Tasks;
-using System.Text;
+using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
-using Microsoft.Extensions.Configuration;
+using Client;
 using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Logging;
+using System.Net.Http;
+using System;
 
-namespace Client
+var builder = WebAssemblyHostBuilder.CreateDefault(args);
+builder.RootComponents.Add<App>("#app");
+builder.RootComponents.Add<HeadOutlet>("head::after");
+builder.Services.AddScoped(sp => new HttpClient
 {
-    public class Program
-    {
-        public static async Task Main(string[] args)
-        {
-            var builder = WebAssemblyHostBuilder.CreateDefault(args);
-            builder.RootComponents.Add<App>("#app");
-
-            builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
-
-            await builder.Build().RunAsync();
-        }
-    }
-}
+    BaseAddress = new Uri(builder.HostEnvironment.BaseAddress)
+});
+await builder.Build().RunAsync();
